5661c857013aabb8e7ee30028317ac3f3e74f5fe
Kiran Nepali <itsmekiran44@gmail.com>
2024-05-19 17:58:42 +0545

    fix vercel deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Hero1 from "@/components/website/Hero1";
-import Hero2 from "@/components/website/Hero2";
-import Hero3 from "@/components/website/Hero3";
-import TrekHero from "@/components/website/TrekHero";
+// import Hero1 from "@/components/website/Hero1";
+// import Hero2 from "@/components/website/Hero2";
+// import Hero3 from "@/components/website/Hero3";
+// import TrekHero from "@/components/website/TrekHero";
 import Try from "@/components/website/Try";
-import TryTransition from "@/components/website/TryTransition";
+// import TryTransition from "@/components/website/TryTransition";
 
 export default function Home() {
   return (
